55a34657c3fd9f003cc87dd882e5cd7a9fa6c951
BloodBlister <peterlittleboy@hotmail.co.uk>
2025-02-10 21:57:39 +0000

    add word count function

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello world")
+
+def main():
+    book_directory = "books/frankenstein.txt"
+    book_text = open_book(book_directory)
+    book_word_count = word_count(book_text)
+    print (book_text)
+    print (f"Word count: {book_word_count}")
+
+
+def open_book(directory):
+    with open(directory) as f:
+        file_contents = f.read()
+    return (file_contents)
+
+def word_count(text):
+    split_words = text.split()
+    num_words = 0
+    for word in split_words:
+        num_words += 1
+    return (num_words)
+
+main()
